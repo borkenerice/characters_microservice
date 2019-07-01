42ba20208b32d7f8a034e13e2270a1bea18ebb82
Borken <erice.borja@hotmail.com>
2019-07-01 11:01:46 +0200

    rename to character from persons

---
+++ api/character.py
@@ -0,0 +1,55 @@
+from flask import abort
+from sqlalchemy.exc import IntegrityError
+
+from api import db
+from api.models import Character, CharacterSchema
+
+
+def read_all_characters():
+    characters = Character.query.order_by(Character.name).all()
+    character_schema = CharacterSchema(many=True)
+    data = character_schema.dump(characters).data
+    return data
+
+
+def read_character(character_id):
+    character = Character.query.get_or_404(character_id, description=f'Character not found with the id: {character_id}')
+    character_schema = CharacterSchema()
+    data = character_schema.dump(character).data
+    return data
+
+
+def update_character(character_id, character_data):
+    character = Character.query.get_or_404(character_id, description=f'Character not found with the id: {character_id}')
+    character_schema = CharacterSchema()
+    try:
+        updated_character = character_schema.load(character_data, session=db.session).data
+        updated_character.character_id = character.character_id
+        db.session.merge(updated_character)
+        db.session.commit()
+        data = character_schema.dump(updated_character).data
+        return data, 201
+    except IntegrityError as i:
+        db.session.rollback()
+        abort(400, f'Character: {character_id} could not be updated: {i.orig}')
+
+
+def create_character(character_data):
+    try:
+        schema = CharacterSchema()
+        new_character = schema.load(character_data, session=db.session).data
+        db.session.add(new_character)
+        db.session.commit()
+        data = schema.dump(new_character).data
+        return data, 201
+    except IntegrityError as i:
+        db.session.rollback()
+        abort(400, f'Character could not be created: {i.orig}')
+
+
+def delete_character(character_id):
+    character = Character.query.get_or_404(character_id, description=f'Character not found with the id: {character_id}')
+    db.session.delete(character)
+    db.session.commit()
+    return
+
--- api/models.py
+++ api/models.py
@@ -1,15 +1,15 @@
 from api import db, ma
 
 
-class Person(db.Model):
+class Character(db.Model):
     __tablename__ = "person"
-    person_id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     place_id = db.Column(db.Integer, nullable=False)
     king = db.Column(db.Boolean, nullable=False)
     alive = db.Column(db.Boolean, nullable=False, default=False)
 
 
-class PersonSchema(ma.ModelSchema):
+class CharacterSchema(ma.ModelSchema):
     class Meta:
-        model = Person
+        model = Character
