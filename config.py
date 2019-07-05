0c499e56c1de13e51097535f56d2825f80826a0b
Borken <berice@aliasrobotics.com>
2019-07-05 17:58:19 +0200

    fix database and service name, use f strings

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ TESTING = True
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-POSTGRES_DEFAULT_USER = 'postgresql'
 POSTGRES_USER = 'characters_micro'
 POSTGRES_PASSWORD = 'characters_micro'
-POSTGRES_DB = 'characters_micro-db'
-SQLALCHEMY_DATABASE_URI = 'postgresql://' + POSTGRES_USER + ':' + POSTGRES_PASSWORD + '@postgresql:5432/' + POSTGRES_DB
-
+POSTGRES_DB = 'characters_micro_db'
+POSTGRES_SERVICE = 'postgres_characters'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVICE}:5432/{POSTGRES_DB}'
 
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
