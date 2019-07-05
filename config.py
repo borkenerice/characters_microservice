f920d9f18e29cc227bd788b3f6d2b5bad716e33e
borkenerice <47315288+borkenerice@users.noreply.github.com>
2019-07-05 18:03:47 +0200

    Fixes in DockerFiles (#2)

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ TESTING = True
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Build the Sqlite ULR for SqlAlchemy
-if os.name == 'nt':
-    sqlite_url = "sqlite:///" + os.path.join(BASE_DIR, "characters.db")
-else:
-    sqlite_url = "sqlite:////" + os.path.join(BASE_DIR, "characters.db")
+POSTGRES_USER = 'characters_micro'
+POSTGRES_PASSWORD = 'characters_micro'
+POSTGRES_DB = 'characters_micro_db'
+POSTGRES_SERVICE = 'postgres_characters'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVICE}:5432/{POSTGRES_DB}'
 
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = sqlite_url
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SWAGGER_DIR = os.path.join(BASE_DIR, 'api', 'swagger.yml')
